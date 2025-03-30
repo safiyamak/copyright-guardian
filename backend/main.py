c401aae166d00b31a796f576e5e00d4e03bc988e
safiyamak <smakada@uwaterloo.ca>
2025-03-30 08:58:55 -0400

    temp

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,7 @@ from ic.identity import Identity
 from ic.candid import encode, decode
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware  # Add this import
-
+from PIL import Image
 import tensorflow as tf
 import os
 from fastapi import FastAPI, File, Form, UploadFile
@@ -13,6 +13,8 @@ from dotenv import load_dotenv
 import json
 import base64
 from perplexity_api import query_perplexity_sonar, image_to_base64, preprocess_image
+from ai_art_similarity import search_with_query_image
+import io
 app = FastAPI()
 load_dotenv()
 
@@ -64,37 +66,22 @@ async def reverse_image_search(
     file: UploadFile = File(...),
 ):
     try:
-        prompt: str = "Please perform a reverse image search on this and provide relevant information about it."
-        print("Received file:", file.filename)
-        print("Received prompt:", prompt)
-        api_key = os.getenv("PERPLEXITY_API_KEY")
-        if not api_key:
-            raise ValueError("PERPLEXITY_API_KEY not found in environment variables")
         # Read the image file
         image_data = await file.read()
-        
-        # Get the base64 encoding of the image
-        image_base64 = image_to_base64(image_data)
         
         # Optional: Get the image embedding if needed for additional processing
         # img_array = preprocess_image(image_data)
         # embedding = image_model.predict(img_array)
         
         # Query Perplexity Sonar API
-        sonar_response = query_perplexity_sonar(api_key, image_base64, prompt)
-        if not sonar_response:
-            raise ValueError("Empty response from Perplexity API")
-        print((f"API Response: {json.dumps(sonar_response, indent=2)}"))
-        # Return the response
-        return JSONResponse(content={
-            "sonar_response": sonar_response,
-        })
-        
+        result = search_with_query_image()
     except Exception as e:
+        print(e)
         return JSONResponse(
             status_code=500,
             content={"error": str(e)}
         )
+    return result
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="127.0.0.1", port=8000)
