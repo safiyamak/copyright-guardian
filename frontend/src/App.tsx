e94072634b6267209e2c6dc64569ae1f4997086b
safiyamak <smakada@uwaterloo.ca>
2025-03-30 09:17:44 -0400

    one char??

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import MainLayout from "./components/layout/MainLayout";
+import MainLayout from "@/components/layout/MainLayout";
 import Dashboard from "./pages/Dashboard";
 import BlockchainSocial from "./pages/BlockchainSocial";
 import ArtTheftDetection from "./pages/ArtTheftDetection";
